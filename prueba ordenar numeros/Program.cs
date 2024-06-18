c9f9dc06c9f6b0782a45e51da9c4f1fa6bd9e980
Jose14Andres <167485194+Jose14Andres@users.noreply.github.com>
2024-06-18 16:00:11 -0500

    Agregar archivos de proyecto.

---
+++ prueba ordenar numeros/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la cantidad de números que desea ordenar: ");
+        int cantidadNumeros = Convert.ToInt32(Console.ReadLine());
+
+        int[] numeros = new int[cantidadNumeros];
+
+        Console.WriteLine("Ingrese los números:");
+        LeerNumeros(numeros);
+
+        OrdenarNumeros(numeros);
+
+        ImprimirNumeros(numeros);
+
+        Console.WriteLine("\nPresione cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+
+    static void LeerNumeros(int[] numeros)
+    {
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            numeros[i] = numero;
+        }
+    }
+
+    static void OrdenarNumeros(int[] numeros)
+    {
+        for (int i = 0; i < numeros.Length - 1; i++)
+        {
+            for (int j = 0; j < numeros.Length - 1 - i; j++)
+            {
+                if (numeros[j] > numeros[j + 1])
+                {
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void ImprimirNumeros(int[] numeros)
+    {
+        Console.WriteLine("\nNúmeros ordenados:");
+        foreach (int numero in numeros)
+        {
+            Console.Write(numero + " ");
+        }
+    }
+}
